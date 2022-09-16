9f9a56215ec619a9558c3be8a69ac3d0da635b60
Vidunram A R <64352864+Goose-Of-War@users.noreply.github.com>
2022-09-16 19:35:41 +0530

    And the token reaches home
    Yay

--- ludo.js
+++ ludo.js
@@ -8,6 +8,7 @@ let TOKEN = {
 // making a map(?)
 let MAP = {};
 ["R", "G", "B", "Y"].forEach(clr => {
+	// normal map positions
 	for (let i=0; i<13; i++) {
 		MAP[clr + i] = {
 			occupants: [],
@@ -27,8 +28,29 @@ let MAP = {};
 			MAP[clr + i].col = i<5 ? 7 : (i<11 ? 11-i : 1);
 		}
 	}
+	// corridoor to ascension
+	for (let i=95; i<100; i++) {
+		MAP[clr + i] = {
+			occupants: [],
+			safety: true
+		};
+		if (clr=="R") {
+			MAP[clr + i].row = 8;
+			MAP[clr + i].col = i-93;
+		} else if (clr=="G") {
+			MAP[clr + i].row = i-93;
+			MAP[clr + i].col = 8;
+		} else if (clr=="B") {
+			MAP[clr + i].row = 8;
+			MAP[clr + i].col = 109-i;
+		} else if (clr=="Y") {
+			MAP[clr + i].row = 109-i;
+			MAP[clr + i].col = 8;
+		}
+	}
 })
 function token_update() {
+	let LOCN = TOKEN.locn;
 	// run loop till dice val runs out to zero
 	while (dice_val--) {
 		// remove from the current tile
@@ -36,13 +58,24 @@ function token_update() {
 		MAP[TOKEN.locn].elm.removeChild(TOKEN.elm);
 		// see what the next tile is
 		if (TOKEN.locn === "Y11") {
-			
+			TOKEN.locn = "R95";
+		} else if (~~(TOKEN.locn.slice(1,)) == 99) {
+			if (dice_val) { TOKEN.locn = LOCN; } else {
+				dice_val = 0;
+				document.getElementById("central-end").appendChild(TOKEN.elm);
+				TOKEN.elm.removeEventListener("click", token_update);
+				return 60053;
+			}
 		} else if (~~(TOKEN.locn.slice(1,))+1 == 13) {
 			TOKEN.locn = ["R", "G", "B", "Y"][(["R", "G", "B", "Y"].findIndex(elm => elm === TOKEN.locn[0])+1)%4]+"0";
 		} else { TOKEN.locn = TOKEN.locn.slice(0,1) + (~~(TOKEN.locn.slice(1,))+1); }
 		// send to the next tile
 		MAP[TOKEN.locn].occupants.push(TOKEN.elm);
 		MAP[TOKEN.locn].elm.appendChild(TOKEN.elm);
+		if (TOKEN.locn === LOCN) {
+			dice_val = -1;
+			break;
+		}
 	}
 	// so that dice_val becomes 0
 	dice_val++;
@@ -52,15 +85,18 @@ function token_update() {
 document.getElementById("init-button").onclick = () => {
 	let container = document.createElement("div");
 	container.id = "ludo-container";
+	let end = document.createElement("div");
+	end.id = "central-end";
+	container.appendChild(end);
 	for (let locn in MAP) {
 		// placing all places on the grid
 		MAP[locn].elm = document.createElement("div");
 		MAP[locn].elm.id = locn;
 		MAP[locn].elm.className = "tile";
-		MAP[locn].elm.innerHTML = locn;
+		//MAP[locn].elm.innerHTML = locn;
 		MAP[locn].elm.style.gridRow = MAP[locn].row;
 		MAP[locn].elm.style.gridColumn = MAP[locn].col;
-		MAP[locn].elm.style.backgroundColor = locn[1]%8 ? "silver" : ["red", "green", "blue", "yellow"].find(color => color[0].toUpperCase() === locn[0]);
+		MAP[locn].elm.style.backgroundColor = (locn[1]%8 && ~~(locn.slice(1,))<=90) ? "silver" : ["red", "green", "blue", "yellow"].find(color => color[0].toUpperCase() === locn[0]);
 		// test case: starting at R0; will be edited to generic case or something else in the future
 		if (locn == "R0") {
 			let token = document.createElement("div");
